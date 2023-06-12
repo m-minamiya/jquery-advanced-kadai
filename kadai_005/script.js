cd1e86a509e1f1c62c7de1a270ee268b319a29ee
mminamiya <minimamguma.miya@gmail.com>
2023-06-12 14:21:15 +0900

    kadai_005実装2回目

--- kadai_005/script.js
+++ kadai_005/script.js
@@ -23,12 +23,36 @@ $(function () {
     });
 
     //AjaxでSTATIC FORMSにデータを送信
-    $("submit").on("click", function (event) {
+    $("#submit").on("click", function (event) {
         //formタグによる送信を拒否
         event.preventDefault();
 
         //入力チェックをした結果、エラーがあるかないか判定
         let result = inputCheck();
+
+        //エラー判定とメッセージを取得
+        let error = result.error;
+        let message = result.message;
+
+        //エラーが無かったらフォームを送信する
+        if (error == false) {
+            //Ajaxでformを送信する
+            $.ajax({
+                url: "https://api.staticforms.xyz/submit",
+                type: "POST",
+                dataType: "json",
+                data: $("#form").serialize(),
+                success: function (result) {
+                    alert("お問い合わせを送信しました。")
+                },
+                error: function (xhr, resp, text) {
+                    alert("お問い合わせを送信できませんでした。")
+                }
+            })
+        } else {
+            //エラーメッセージを表示する
+            alert(message);
+        }
     });
 
     //フォーカスが外れたとき(blur)にフォームの入力チェックをする
@@ -45,6 +69,10 @@ $(function () {
         inputCheck();
     });
     $("#message").blur(function () {
+        inputCheck();
+    });
+    //課題
+    $("#prefecture").blur(function () {
         inputCheck();
     });
     $("#agree").click(function () {
@@ -65,9 +93,91 @@ $(function () {
         //お名前のチェック
         if ($("#name").val() == "") {
             //エラーあり
+            $("#name").css("background-color", "#f79999");
+            error = true;
+            message += "お名前を入力してください。\n";
         }　else {
             //エラーなし
+            $("#name").css("background-color", "#fafafa");
         }
+        // フリガナのチェック
+        if ($("#furigana").val() == "") {
+            // エラーあり
+            $("#furigana").css("background-color", "#f79999");
+            error = true;
+            message += "フリガナを入力してください。\n";
+        } else {
+            // エラーなし
+            $("#furigana").css("background-color", "#fafafa");
+        }
+  
+        // お問い合わせのチェック
+        if ($("#message").val() == "") {
+            // エラーあり
+            $("#message").css("background-color", "#f79999");
+            error = true
+            message += "お問い合わせ内容を入力してください。\n";
+        } else {
+            // エラーなし
+            $("#message").css("background-color", "#fafafa");
+        }
+
+        //メールアドレスのチェック
+        if ($("#email").val() == "" || $("#email").val().indexOf("@") == -1 || $("#email").val().indexOf(".") == -1) {
+            //エラーあり
+            $("#email").css("background-color", "#f79999");
+            error = true;
+            message += "メールアドレスが未入力、または「@」「.」が含まれていません。\n";
+        } else {
+            //エラーなし
+            $("#email").css("background-color", "#fafafa");
+        }
+
+        //電話番号のチェック(未入力はOK、未入力で無い場合は-が必要)
+        if ($("#tel").val() != "" && $("#tel").val().indexOf("-") == -1) {
+            //エラーあり
+            $("#tel").css("background-color", "#f79999");
+            error = true;
+            message += "電話番号に「-」が含まれていません。\n";
+        } else {
+            //エラーなし
+            $("#tel").css("background-color", "#fafafa");
+        }
+
+        //課題
+        //都道府県のチェック
+        if ($("#prefecture").val() == "") {
+            //エラーあり
+            $("#prefecture").css("background-color", "#f79999");
+            error = true;
+            message = "「都道府県」を選択してください。\n";
+        } else {
+            //エラーなし
+            $("#prefecture").css("background-color", "#fafafa");
+        }
+
+        //質問
+        //個人情報のチェックボックスのチェック
+        if ($("#agree").prop("checked") == false) {
+            error = true;
+            message += "個人情報の取り扱いについてご同意いただける場合は、チェックボックスにチェックしてください。\n";
+        }
+
+        //エラーの有無で送信ボタンを切り替え
+        if (error == true) {
+            $("#submit").attr("src", "images/button-submit.png");
+        } else {
+            $("#submit").attr("src", "images/button-submit-blue.png");
+        }
+
+        //オブジェクトでエラー判定とメッセージを返す
+        result = {
+            error: error,
+            message: message
+        }
+
+        //戻り値としてエラーがあるかどうかを返す
+        return result;
     }
 });
     
